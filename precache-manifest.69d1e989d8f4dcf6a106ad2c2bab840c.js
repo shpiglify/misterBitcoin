ad417a5db28acbbeeb65ecf26505ced568190cbb
guy20056 <guy20056@walla.com>
2019-05-20 20:31:46 +0300

    Updates

---
+++ precache-manifest.69d1e989d8f4dcf6a106ad2c2bab840c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d60c9648a2f0a0e8b6ed6eb49e4832fb",
+    "url": "/react-bitCoin/static/media/edit.d60c9648.png"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/react-bitCoin/static/media/Quando-Regular.849e6795.ttf"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/react-bitCoin/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "4c7cd4aa705669c58d60",
+    "url": "/react-bitCoin/static/js/runtime~main.5a36ab58.js"
+  },
+  {
+    "revision": "a9a7f4b4520fbeea141f",
+    "url": "/react-bitCoin/static/js/main.a5e0153d.chunk.js"
+  },
+  {
+    "revision": "4b7caa24489129411904",
+    "url": "/react-bitCoin/static/js/2.de91f0fc.chunk.js"
+  },
+  {
+    "revision": "a9a7f4b4520fbeea141f",
+    "url": "/react-bitCoin/static/css/main.c40386a3.chunk.css"
+  },
+  {
+    "revision": "1231f6d8449da92251078585212e056d",
+    "url": "/react-bitCoin/index.html"
+  }
+];
