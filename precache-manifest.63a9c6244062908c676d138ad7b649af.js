2b3bb48ce9a3914756f914282fb4abb72e4a5180
guy20056 <guy20056@walla.com>
2019-05-20 20:56:00 +0300

    Updates

---
+++ precache-manifest.63a9c6244062908c676d138ad7b649af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d60c9648a2f0a0e8b6ed6eb49e4832fb",
+    "url": "/misterBitcoin/static/media/edit.d60c9648.png"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/misterBitcoin/static/media/Quando-Regular.849e6795.ttf"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/misterBitcoin/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "dfae38a9b64f5b3ecd0c",
+    "url": "/misterBitcoin/static/js/runtime~main.55af8474.js"
+  },
+  {
+    "revision": "cb1e5caa9e1f9b099eb6",
+    "url": "/misterBitcoin/static/js/main.cdf917ba.chunk.js"
+  },
+  {
+    "revision": "1310adfc68a26aa379ef",
+    "url": "/misterBitcoin/static/js/2.6f7798ec.chunk.js"
+  },
+  {
+    "revision": "cb1e5caa9e1f9b099eb6",
+    "url": "/misterBitcoin/static/css/main.d0b90f03.chunk.css"
+  },
+  {
+    "revision": "fedd74c111cc51a68277500cb9c8cccd",
+    "url": "/misterBitcoin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/misterBitCoin/precache-manifest.9a7fa54f094cba62417e6721f511ebf9.js"
+  "/misterBitcoin/precache-manifest.63a9c6244062908c676d138ad7b649af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/misterBitCoin/index.html", {
+workbox.routing.registerNavigationRoute("/misterBitcoin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
